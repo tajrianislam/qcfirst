f2cb0ba733db0ba9d43aaafbfc4bee8f3379b335
Daven <daven.santana38@qmail.cuny.edu>
2021-04-30 22:34:14 -0400

    Signup fully works

--- qcfirstCode/js/database.js
+++ qcfirstCode/js/database.js
@@ -1,4 +1,5 @@
 const mongoose = require ('mongoose');
+require('mongoose-type-email');
 
 mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
     if (err) throw err;
@@ -11,12 +12,13 @@ const studentSchema = new Schema ({
     lastName: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    home: {type: String},
 });
 
 const instructorSchema = new Schema ({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: mongoose.SchemaTypes.Email, required: true},
     password: {type: String, required: true},
 });
 
@@ -24,8 +26,15 @@ const Student = mongoose.model("Student", studentSchema);
 const Instructor = mongoose.model("Instructor", instructorSchema);
 
 
-const createAndSaveStudent = (done, fName, lName, emailAddress, pass) => {
-    var student = new Student({firstName: fName, lastName: lName, email: emailAddress, password: pass});
+const createAndSaveStudent = (fName, lName, emailAddress, confirmEmailAddress, pass, confirmPass, done) => {
+
+    if(emailAddress != confirmEmailAddress)
+        return console.error("Emails do not match");
+
+    if(pass != confirmPass)
+        return console.error("Passwords do not match");
+    
+    var student = new Student({firstName: fName, lastName: lName, email: emailAddress, password: pass, home: "/studenthome"});
 
     student.save(function(err, data) {
         if (err) 
@@ -35,7 +44,14 @@ const createAndSaveStudent = (done, fName, lName, emailAddress, pass) => {
     });
 }
 
-const createAndSaveInstructor = (done, fName, lName, emailAddress, pass) => {
+const createAndSaveInstructor = (fName, lName, emailAddress, confirmEmailAddress, pass, confirmPass, done) => {
+
+    if(emailAddress != confirmEmailAddress)
+        return console.error("Emails do not match");
+
+    if(pass != confirmPass)
+        return console.error("Passwords do not match");
+    
     var instructor = new Instructor({firstName: fName, lastName: lName, email: emailAddress, password: pass});
 
     instructor.save(function(err, data) {
@@ -46,31 +62,36 @@ const createAndSaveInstructor = (done, fName, lName, emailAddress, pass) => {
     });
 }
 
-const findUserLogin = (done, emailAddress, pass) => {
+const findUserLogin = (emailAddress, pass, done) => {
+
+    //Figure out how to authenticate user
     
-    Student.count({email: emailAddress, password: pass}, function (err, count){ 
-        if(count > 0){
-            Student.find({email: emailAddress, password: pass}, (error, student) => {
-                document.querySelector("#studentName").innerHTML = student.firstName + " " + student.lastName;
-                document.querySelector("#studentID").innerHTML = student._id;
-                done(null, data)
-              });
-        }
-    });
+    // Student.countDocuments({email: emailAddress, password: pass}, function (err, count){ 
+       
+    //     if (err) return console.error(err);
+        
+    //     if(count > 0){
+    //         Student.find({email: emailAddress, password: pass}, (error, student) => {
+
+    //             if (err) return console.error(err);
+                
+    //             console.log("Student login");       
+    //             done(null, student);
+    //         });
+    //     }
+    // });
+
+    // Instructor.countDocuments({email: emailAddress, password: pass}, function (err, count){ 
+    //     if(count > 0){
+    //         Student.find({email: emailAddress, password: pass}, (error, student) => {
+    //             return console.log("Login Successful");
+    //         });
+    //     }
+    // }); 
     
-    Instructor.count({email: emailAddress, password: pass}, function (err, count){ 
-        if(count > 0){
-            Student.find({email: emailAddress, password: pass}, (error, student) => {
-                document.querySelector("#instructorName").innerHTML = student.firstName + " " + student.lastName;
-                document.querySelector("#instructorID").innerHTML = student._id;
-                done(null, data)
-              });
-        }
-    }); 
-    
-    document.querySelector("#invalid").innerHTML = "Login Failed";
-    done(null, data);
+    //return console.error("Login Failed");
 }
+
 
 exports.StudentModel = Student;
 exports.InstructorModel = Instructor;
--- qcfirstCode/js/server.js
+++ qcfirstCode/js/server.js
@@ -18,31 +18,31 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'index.html'));
 });
 
-app.get('/signup.html', (req, res) => {
+app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'signup.html'));
 });
 
-app.get('/instructorcreate.html', (req, res) => {
+app.get('/instructorcreate', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'instructorcreate.html'));
 });
 
-app.get('/instructorhome.html', (req, res) => {
+app.get('/instructorhome', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'instructorhome.html'));
 });
 
-app.get('/instructorroster.html', (req, res) => {
+app.get('/instructorroster', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'instructorroster.html'));
 });
 
-app.get('/studentcourses.html', (req, res) => {
+app.get('/studentcourses', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'studentcourses.html'));
 });
 
-app.get('/studentenroll.html', (req, res) => {
+app.get('/studentenroll', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'studentenroll.html'));
 });
 
-app.get('/studenthome.html', (req, res) => {
+app.get('/studenthome', (req, res) => {
     res.sendFile(path.join(__dirname, '../html', 'studenthome.html'));
 });
 
@@ -50,11 +50,44 @@ app.listen(port, () => {
     console.log(`Listening on Port:${port}`)
 });
 
-app.post('/', function(req, res) {
-    //Use findUserLogin function
-})
-
+const findUserLogin = require("./database.js").findUserLogin;
 app.post('/login', function(req, res) {
-    //Use createAndSave Student or Instructor based off button pressed
-    //Then send them back to login to log in
+    
+    var email = req.body.emailLogin;
+    var password = req.body.passwordLogin;
+
+    findUserLogin(email, password, function(err, user) {
+
+        //First find out whether the user is a student or instructor or if it doesn't exist
+        //Then send the user to the home page depending on what kind of user they are
+        //Javascript should change the html so that in the name appears the name signed up with
+        //ID should be object ID and done
+    
+    });
+
+
+});
+
+const createAndSaveStudent = require("./database.js").createAndSaveStudent;
+const createAndSaveInstructor = require("./database.js").createAndSaveInstructor;
+app.post('/signup', function(req, res) {
+
+    var firstName = req.body.signupFirstName;
+    var lastName = req.body.signupLastName;
+    var email = req.body.signupEmail;
+    var confirmEmail = req.body.signupConfirmEmail;
+    var password = req.body.signupPassword;
+    var confirmPassword = req.body.signupConfirmPassword;
+
+    if(req.body.signupButton == 1){
+        createAndSaveStudent(firstName, lastName, email, confirmEmail, password, confirmPassword, function (err, data) {
+            if(err) return(err);
+            res.sendFile(path.join(__dirname, '../html', 'studenthome.html'));
+        });
+    } else {
+        createAndSaveInstructor(firstName, lastName, email, confirmEmail, password, confirmPassword, function (err, data) {
+            if(err) return(err);
+            res.sendFile(path.join(__dirname, '../html', 'instructorhome.html'));
+        });
+    }
 })
